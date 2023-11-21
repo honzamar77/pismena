cc41a14adb26470b3ae528f0101fcf243672b861
honza <Hota2012cz@gmail.com>
2023-11-21 18:29:38 +0100

    ahoj tady je gappa vole

---
+++ napis.py
@@ -0,0 +1,22 @@
+from abeceda import *
+
+pismeno_z()
+
+
+a=textinput(title="", prompt=:"Zadej slovo: ")
+
+def mezera():
+    penup()
+    forward(20)
+    pendown()
+
+for pismeno in slovo:
+    if pismeno == "a":
+        pismeno_a()
+        
+
+
+
+
+
+
