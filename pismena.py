f231471d86e7e9e26fa197d00e733ccfc3a22f83
honza <Hota2012cz@gmail.com>
2023-11-21 17:09:23 +0100

    Initial commit

---
+++ pismena.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+
+def pismeno_t():
+  left(90)
+  forward(100)
+  right(90)
+  forward(25)
+  left(180)
+  forward(50)
+
+  left(180)
+  forward(25)
+  right(90)
+  forward(100)
+
+
+
+
+
+
+
+    # tady nakreslit zbytek
+
+
+pismeno_t()
+exitonclick()
